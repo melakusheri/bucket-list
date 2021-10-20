45ef4a958be5792f96e563024bbd4459a9a2f9e3
Melaku Sheri <mailangels@gmail.com>
2021-10-19 21:58:35 -0400

    bucket.js modified

--- src/index.js
+++ src/index.js
@@ -1,10 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
+
+function App() {
+    return ( <
+        div >
+        <
+        ToDoList / >
+        <
+        /div>
+    );
+}
+
+function ToDoList() {
+    const [text, setText] = React.useState("");
+    const [todos, setTodos] = React.useState([]);
+    const [finishedTodos, setFinishedTodos] = React.useState([]);
+
+    function addTodo() {
+        setTodos([...todos, text]);
+        setText("");
+    }
+
+    function finishTodo(todo) {
+        setFinishedTodos([...finishedTodos, todo]);
+    }
+
+    function startTodo(todo) {
+        setFinishedTodos(
+            finishedTodos.filter(finishedTodo => finishedTodo !== todo)
+        );
+    }
+
+    return ( <
+        div className = "body" >
+        <
+        div className = "list" >
+        <
+        h1 > Bucket List < /h1> {
+            todos.map(todo => {
+                    const isDone = finishedTodos.includes(todo);
+                    return ( <
+                        h3 key = { todo } >
+                        <
+                        input type = "checkbox"
+                        checked = { isDone }
+                        onChange = {
+                            function(e) {
+                                isDone ? startTodo(todo) : finishTodo(todo);
+                            }
+                        }
+                        /> {
+                            isDone ? < del > { todo } < /del> : todo}   <
+                                /h3>
+                        );
+                    })
+            }
+
+            <
+            input
+            value = { text }
+            onChange = {
+                function(e) {
+                    setText(e.target.value);
+                }
+            }
+            />
+
+            <
+            button disabled = {!text }
+            onClick = { addTodo } >
+                Add!
+                <
+                /button> <
+                /div>   <
+                /div>
+        );
+    }
+
+    const rootElement = document.getElementById("root");
+    ReactDOM.render( < App / > , rootElement);
